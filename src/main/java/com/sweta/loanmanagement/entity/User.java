f193729927258836fd6e788626ba353c12e754a8
Sweta-self <swetaa.kumari9@gmail.com>
2025-12-25 15:33:06 +0530

    add user entity and role based auth model

---
+++ src/main/java/com/sweta/loanmanagement/entity/User.java
@@ -0,0 +1,57 @@
+package com.sweta.loanmanagement.entity;
+
+import com.sweta.loanmanagement.enums.Role;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false,unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public User(Long id, String email, String password, Role role) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
---
+++ src/main/java/com/sweta/loanmanagement/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.sweta.loanmanagement.repository;
+
+import com.sweta.loanmanagement.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+}
